776a6e5bd2baa711af759189507366dc9c9ac928
Miggy Bagadiong <miggy.0825@gmail.com>
2024-03-05 21:20:19 +0800

    commit first project
    Hello World project file

---
+++ First_HelloWorld/HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Name
+    {
+        static void Main(string[] args)
+        {
+            // display the first part
+            string displayFirstQuestion = "What is your name? ";
+            Console.WriteLine(displayFirstQuestion);
+            // give the input for the thing
+            string inputName = Console.ReadLine();
+            //clears the thing
+            Console.Clear();
+            // display the name
+            Console.WriteLine("Hello " +  inputName + " !");
+
+
+        }
+    }
+    
+}
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
