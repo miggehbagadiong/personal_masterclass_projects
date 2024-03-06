17070cb9cf1f173420620d5ff4577b8daf08bbfe
Miggy Bagadiong <miggy.0825@gmail.com>
2024-03-06 17:18:19 +0800

    commit old style application
    this uses implementation with the beep sound

---
+++ First_HelloWorld/Old Style App/OldStyleApplication/OldStyleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OldStyleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.ReadLine(); // reads the data in the next line. once entered, it will be stored
+            //Console.Beep();
+
+            Console.Beep();
+            Console.WriteLine("Starting!");
+
+            Console.Clear();
+            Console.WriteLine("Hello What is your name?");
+            string name = Console.ReadLine();
+            Console.Beep();
+
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine("Hello " + name + "!");
+
+        }
+    }
+}
